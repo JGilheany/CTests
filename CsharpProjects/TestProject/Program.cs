33dd24eeddd84a427dcdfd45c41fb837ab486861
JGilheany <joshgilheany@gmail.com>
2023-08-16 14:56:56 +0100

    Switchupdate

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -108,28 +108,89 @@
 
 // Console.WriteLine($"Total: {total}");
 
-int employeeLevel = 100;
-string employeeName = "John Smith";
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
 
-string title = "";
+// string title = "";
 
-switch (employeeLevel)
+// switch (employeeLevel)
+// {
+//     case 100:
+//         title = "Junior Associate";
+//         break;
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+
+// Console.WriteLine($"{employeeName}, {title}");
+
+
+
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-    case 100:
-        title = "Junior Associate";
+    case "01":
+        type = "Sweat shirt";
         break;
-    case 200:
-        title = "Senior Associate";
+    case "02":
+        type = "T-shirt";
         break;
-    case 300:
-        title = "Manager";
-        break;
-    case 400:
-        title = "Senior Manager";
+    case "03":
+        type = "Sweat pants";
         break;
     default:
-        title = "Associate";
+        type = "Other";
         break;
 }
 
-Console.WriteLine($"{employeeName}, {title}");
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
