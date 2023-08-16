1716f4421d645ab68ca458fcb9b7d2a4f8521a13
JGilheany <joshgilheany@gmail.com>
2023-08-16 13:43:50 +0100

    Switches

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -27,7 +27,7 @@
 // for (int ctr = 0; ctr <= 4; ctr++)
 //     Console.Write(ctr);
 //     Console.Write("{0,8:N0}", rand.Next(2));
-    
+
 // Console.WriteLine();
 
 // var rand = new Random();
@@ -84,26 +84,52 @@
 
 
 
-using System;
+// using System;
 
-int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+// int total = 0;
 
-foreach (int number in numbers)
+// foreach (int number in numbers)
+// {
+
+//     total += number;
+
+//     if (number == 42)
+//     {
+//         bool found = true;
+
+//         if (found)
+//         {
+//             Console.WriteLine("Set contains 42");
+//         }
+
+//     }
+// }
+
+// Console.WriteLine($"Total: {total}");
+
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
 {
-
-    total += number;
-
-    if (number == 42)
-    {
-        bool found = true;
-
-        if (found)
-        {
-            Console.WriteLine("Set contains 42");
-        }
-
-    }
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
 }
 
-Console.WriteLine($"Total: {total}");
+Console.WriteLine($"{employeeName}, {title}");
